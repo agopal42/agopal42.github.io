933b300f1c1e43232aa997fd4fedae8a03bf0665
agopal42 <53858515+agopal42@users.noreply.github.com>
2025-11-21 02:31:35 +0000

    Deploying to gh-pages from @ agopal42/agopal42.github.io@697a2112eb7ecf55cd883c33dc474b621de4aeb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,7 +445,7 @@ ninja.data = [{
         title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("https://bsky.app/profile/agopal42", "_blank");
+          window.open("https://bsky.app/profile/agopal42.bsky.social", "_blank");
         },
       },{
         id: 'social-cv',
